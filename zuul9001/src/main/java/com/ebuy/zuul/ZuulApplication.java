e79ffc8868a8546cc5bb5c660b61456863bc5aa0
yjm <932786235@qq.com>
2020-06-03 20:21:43 +0800

    提交网关模块
    Signed-off-by: yjm <932786235@qq.com>

---
+++ zuul9001/src/main/java/com/ebuy/zuul/ZuulApplication.java
@@ -0,0 +1,16 @@
+package com.ebuy.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class ZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(ZuulApplication.class);
+        application.run(args);
+    }
+}
---
+++ zuul9001/src/main/java/com/ebuy/zuul/filter/MyFilter.java
@@ -0,0 +1,37 @@
+package com.ebuy.zuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class MyFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return FilterConstants.PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        // 此时执行可以拿到登录信息url等
+        return FilterConstants.PRE_DECORATION_FILTER_ORDER - 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext currentContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = currentContext.getRequest();
+        String remoteAddr = request.getRemoteAddr();
+        System.out.println("访问者IP："+remoteAddr+"访问地址:"+request.getRequestURI());
+        return null;
+    }
+}
